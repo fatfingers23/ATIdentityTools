3b4f3c736f494271317c3d61feeff75ef36ba7c8
Christopher Jr Riley <chrisjr@cjrriley.com>
2025-05-09 03:43:36 -0400

    Remove example test

--- Tests/ATIdentityToolsTests/ATIdentityToolsTests.swift
+++ Tests/ATIdentityToolsTests/ATIdentityToolsTests.swift
@@ -8,7 +8,3 @@
 
 import Testing
 @testable import ATIdentityTools
-
-@Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-}
